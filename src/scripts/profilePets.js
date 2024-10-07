b4841713b8cf303792d1ba9d5eca3615bb0e6ba1
Washington <washington.contato1327@gmail.com>
2024-10-07 15:16:46 -0300

    Iniciando as configurações da pag de perfil de pets

--- src/scripts/profilePets.js
+++ src/scripts/profilePets.js
@@ -1,5 +1,6 @@
 const containerCardsProfile = document.querySelector(".container-cards");
-1;
+const containerProfile = document.querySelector(".container-profile");
+
 async function fetchPets() {
   try {
     const response = await fetch("/src/data/pets.json");
@@ -12,13 +13,10 @@ async function fetchPets() {
 
 function renderPetCardsProfile(pets) {
   containerCardsProfile.innerHTML = "";
+
   pets.forEach((pet) => {
     const card = document.createElement("div");
     card.className = "cards";
-
-    const buttonClass =
-      pet.statusAdocao === "Disponível" ? "btn-available" : "btn-process";
-    const disabledAttr = pet.statusAdocao !== "Disponível" ? "disabled" : "";
 
     card.innerHTML = `
         <div class="img">
@@ -29,14 +27,49 @@ function renderPetCardsProfile(pets) {
         <p>Sexo: ${pet.sexo}</p>
         <p>Idade: ${pet.idade} anos</p>
         <p>Porte: ${pet.porte}</p>
-        <button class="${buttonClass}" ${disabledAttr}>
-            ${pet.statusAdocao}
-        </button>
+        <button class="btn-view-profile">Ver Perfil</button>
     `;
 
     containerCardsProfile.appendChild(card);
+
+    const viewProfileButton = card.querySelector(".btn-view-profile");
+    viewProfileButton.addEventListener("click", () => showPetProfile(pet));
   });
 }
 
-// Inicia a busca dos pets e renderiza os cards
+function showPetProfile(pet) {
+  const profileContent = `
+    <div class="profile-content">
+      <div class="img-profile">
+        <img alt="Foto de ${pet.nome}" src="${pet.foto}" />
+      </div>
+      <h3>Nome do pet: ${pet.nome}</h3>
+      <p>${pet.descricao}</p>
+      <video src="${pet.video}" controls>
+        Seu navegador não suporta vídeos.
+      </video>
+      <button class="btn-back">Voltar</button>
+    </div>
+  `;
+
+  containerProfile.innerHTML = profileContent;
+  containerProfile.style.display = "flex";
+
+  // BOTAO PARA FECHAR MODAL
+  const backButton = containerProfile.querySelector(".btn-back");
+  backButton.addEventListener("click", () => {
+    closeModal();
+  });
+  //FECHAR MODAL CLICANDO FORA
+  containerProfile.addEventListener("click", (event) => {
+    if (event.target === containerProfile) {
+      closeModal();
+    }
+  });
+}
+
+function closeModal() {
+  containerProfile.style.display = "none";
+}
+
 fetchPets();
